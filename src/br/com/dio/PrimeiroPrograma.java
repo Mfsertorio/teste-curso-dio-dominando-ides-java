421a15483e9f774016f0607457e4b94713ab91b6
Mfsertorio <mfsertorio@gmail.com>
2021-11-29 15:50:15 -0300

    Primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.teste;
+
+public class PrimeiroPrograma {
+teste teste = new teste();
+	public static void main(String[] args) {
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World" + (a+b));*/
+	}
+
+}
+class livros {
+	private String name;
+	private String npag;
+}
